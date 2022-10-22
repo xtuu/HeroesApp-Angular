d83f0007564b2fdaa2c19581b74698e0866a1e89
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-10-22 02:27:54 -0400

    rutas hijas LazyLoad

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { RegisterComponent } from './pages/register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
