3c1485f2701566f00f66b9ae887eab33290dd51f
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-10-25 09:23:38 -0400

    Agregar, editar

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,14 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+
+import { Heroe, Publisher } from '../../interfaces/heroes.intercafe';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
+  styles: [`
+  img {
+    width: 100%;
+    border-radius: 5px
+  }`]
 })
+
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  }
+
+  constructor(
+    private heroeServices: HeroesService,
+    private activatedRouter: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+
+    if (!this.router.url.includes('editar')) { return; }
+    this.activatedRouter.params.pipe(
+      switchMap(({ id }) => this.heroeServices.getHeroePorId(id))
+    ).subscribe(heroe => this.heroe = heroe)
+
+  }
+
+
+  guardar() {
+
+    if (this.heroe.superhero.trim().length === 0) { return; }
+
+    if (this.heroe.id) {
+      //actualizar
+      this.heroeServices.editarHeroe(this.heroe)
+        .subscribe(heroe => console.log('Actulizando', heroe))
+    } else {
+      //crear
+      this.heroeServices.agregarHeroe(this.heroe)
+        .subscribe(heroe => {
+          this.router.navigate(['/heroes/editar', heroe.id]);
+        })
+    }
+
+
+
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -26,8 +26,17 @@ export class HeroesService {
 
   getSugerencias(termino: string): Observable<Heroe[]> {
     return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${termino}&_limit=6`)
-
-
   }
+
+
+  agregarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe)
+  }
+
+
+  editarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.put<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`, heroe)
+  }
+
 
 }
